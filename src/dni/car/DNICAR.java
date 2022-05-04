39d1b320b2bd667e177869240b9cfd9bc0afc422
DAYANA_PONCE <DAYANA_PONCE@DESKTOP-DB2INKV>
2022-05-03 23:15:07 -0400

    se copio 5 veces

--- src/dni/car/DNICAR.java
+++ src/dni/car/DNICAR.java
@@ -16,6 +16,11 @@ public class DNICAR {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("PRIMERA PRUEBA");
+        System.out.println("PRIMERA PRUEBA");
+        System.out.println("PRIMERA PRUEBA");
+        System.out.println("PRIMERA PRUEBA");
         System.out.println("PRIMERA PRUEBA");
     }
     
