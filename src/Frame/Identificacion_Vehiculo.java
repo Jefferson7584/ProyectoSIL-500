f2374399b3977b90154f32ba85ae0c42530be7bb
JEFFERSON <JEFFERSON@169.254.74.111>
2022-05-19 14:36:13 -0400

    se cambio del formualrio Identificacion Vehiculo

--- src/Frame/Identificacion_Vehiculo.java
+++ src/Frame/Identificacion_Vehiculo.java
@@ -178,9 +178,7 @@ public class Identificacion_Vehiculo extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 661, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 109, Short.MAX_VALUE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 661, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
