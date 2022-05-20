39880d92f90b286f03309d99c371186a4301b067
JEFFERSON <JEFFERSON@169.254.74.111>
2022-05-20 00:45:32 -0400

    se cambio del formualrio Identificacion Vehiculo

--- src/Frame/Menu_Principal.java
+++ src/Frame/Menu_Principal.java
@@ -5,17 +5,24 @@
  */
 package Frame;
 
+import com.mysql.jdbc.Connection;
+import conexionSQL.conexionSQL;
+
 /**
  *
  * @author JEFFERSON
  */
 public class Menu_Principal extends javax.swing.JFrame {
-
+    
+       conexionSQL cc = new conexionSQL();
+       Connection con = cc.conexion();
     /**
      * Creates new form Menu_Principal
      */
     public Menu_Principal() {
         initComponents();
+        this.setTitle("MENU");
+        this.setLocationRelativeTo(null);
     }
 
     /**
@@ -28,10 +35,11 @@ public class Menu_Principal extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
+        btnPROPIETARIO = new javax.swing.JButton();
+        btnDATOS_TECNICOS = new javax.swing.JButton();
+        btnIDENTIFICACION_VEHICULO = new javax.swing.JButton();
+        btnGENERAR_CARNET = new javax.swing.JButton();
+        btnSALIR = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -39,17 +47,40 @@ public class Menu_Principal extends javax.swing.JFrame {
         jPanel1.setBackground(new java.awt.Color(0, 51, 102));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jButton1.setText("jButton1");
-        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 70, -1, -1));
+        btnPROPIETARIO.setText("PROPIETARIO");
+        btnPROPIETARIO.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnPROPIETARIOActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btnPROPIETARIO, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 80, 170, 50));
 
-        jButton2.setText("jButton2");
-        jPanel1.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 150, -1, -1));
+        btnDATOS_TECNICOS.setText("DATOS TECNICOS");
+        btnDATOS_TECNICOS.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnDATOS_TECNICOSActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btnDATOS_TECNICOS, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 80, 140, 50));
 
-        jButton3.setText("jButton3");
-        jPanel1.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 260, -1, -1));
+        btnIDENTIFICACION_VEHICULO.setText("IDENTIFICACION VEHICULO");
+        btnIDENTIFICACION_VEHICULO.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnIDENTIFICACION_VEHICULOActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btnIDENTIFICACION_VEHICULO, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 200, -1, 60));
 
-        jButton4.setText("jButton4");
-        jPanel1.add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 320, -1, -1));
+        btnGENERAR_CARNET.setText("GENERAR CARNET");
+        btnGENERAR_CARNET.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnGENERAR_CARNETActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btnGENERAR_CARNET, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 200, 140, 60));
+
+        btnSALIR.setText("SALIR");
+        jPanel1.add(btnSALIR, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 320, 120, 40));
         jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 570, 390));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -65,6 +96,34 @@ public class Menu_Principal extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnPROPIETARIOActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPROPIETARIOActionPerformed
+        // TODO add your handling code here:
+        Propietario abrir = new Propietario();
+        abrir.setVisible(true);
+        this.setVisible(false);
+    }//GEN-LAST:event_btnPROPIETARIOActionPerformed
+
+    private void btnDATOS_TECNICOSActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDATOS_TECNICOSActionPerformed
+        // TODO add your handling code here:
+        Datos_Tecnicos abrir = new Datos_Tecnicos();
+        abrir.setVisible(true);
+        this.setVisible(false);
+    }//GEN-LAST:event_btnDATOS_TECNICOSActionPerformed
+
+    private void btnIDENTIFICACION_VEHICULOActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnIDENTIFICACION_VEHICULOActionPerformed
+        // TODO add your handling code here:
+        Identificacion_Vehiculo abrir = new Identificacion_Vehiculo();
+        abrir.setVisible(true);
+        this.setVisible(false);
+    }//GEN-LAST:event_btnIDENTIFICACION_VEHICULOActionPerformed
+
+    private void btnGENERAR_CARNETActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGENERAR_CARNETActionPerformed
+        // TODO add your handling code here:
+        Generar_Carnet abrir = new Generar_Carnet();
+        abrir.setVisible(true);
+        this.setVisible(false);
+    }//GEN-LAST:event_btnGENERAR_CARNETActionPerformed
 
     /**
      * @param args the command line arguments
@@ -102,10 +161,11 @@ public class Menu_Principal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
+    private javax.swing.JButton btnDATOS_TECNICOS;
+    private javax.swing.JButton btnGENERAR_CARNET;
+    private javax.swing.JButton btnIDENTIFICACION_VEHICULO;
+    private javax.swing.JButton btnPROPIETARIO;
+    private javax.swing.JButton btnSALIR;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
